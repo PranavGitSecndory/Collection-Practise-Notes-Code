52c772c8810b594c59c96246f857a17975e0411c
Pranav Chavan <pranavchavan970@gmail.com>
2025-10-03 14:05:35 +0530

    new file added for pract list

---
+++ PractList1.java
@@ -0,0 +1,39 @@
+//package collection_Framework;
+
+import java.util.ArrayList;
+
+public class PractList1 {
+    public static void main(String[] args) {
+         ArrayList<StudentData> sd=new ArrayList<>();
+         sd.add(new StudentData(1, "pranav", 78, 500));
+         sd.add(new StudentData(2, "nikhil", 98, 400));
+         sd.add(new StudentData(3, "suraj", 70, 700));
+         sd.add(new StudentData(4, "abhi", 45, 900));
+
+         System.out.println(sd);   }
+}
+
+class StudentData{
+    int std_id;
+    String std_name;
+    int mks_csc;
+    int mks_mm;
+
+  public StudentData(int std_id,String std_name,int mks_csc,int mks_mm)
+  {
+    super();
+    this.mks_csc=mks_csc;
+    this.std_id=std_id;
+    this.std_name=std_name;
+    this.mks_mm=mks_mm;
+  }
+
+  void showData(){
+
+    System.out.println("student ID::"+std_id+"Studnt name"+std_name+"marks"+mks_csc);
+  }
+
+
+
+
+}
